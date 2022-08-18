1f42131710a973660f6f821ebec62e4c892f40fe
absatish <47278791+absatish@users.noreply.github.com>
2022-08-18 13:49:54 +0530

    Update UIFactory.java

--- src/main/java/com/defectlist/inwarranty/ui/UIFactory.java
+++ src/main/java/com/defectlist/inwarranty/ui/UIFactory.java
@@ -14,6 +14,100 @@ import java.util.stream.Collectors;
 public class UIFactory {
 
 
+    public static String getFirstPageV3() {
+        return "<html>" +
+                "<title>Login Page</title>" +
+                "<meta http-equiv = \"refresh\" content = \"5; url = https://absatish.github.io/defectslist-ui\" />" +
+                "\t\t<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">\n" +
+                "<style>" +
+                ".switch {\n" +
+                "  position: relative;\n" +
+                "  display: inline-block;\n" +
+                "  width: 52px;\n" +
+                "  height: 24px;\n" +
+                "}\n" +
+                "\n" +
+                ".switch input { \n" +
+                "  opacity: 0;\n" +
+                "  width: 0;\n" +
+                "  height: 0;\n" +
+                "}\n" +
+                "\n" +
+                ".slider {\n" +
+                "  position: absolute;\n" +
+                "  cursor: pointer;\n" +
+                "  top: 0;\n" +
+                "  left: 0;\n" +
+                "  right: 0;\n" +
+                "  bottom: 0;\n" +
+                "  background-color: #ccc;\n" +
+                "  -webkit-transition: .4s;\n" +
+                "  transition: .4s;\n" +
+                "}\n" +
+                "\n" +
+                ".slider:before {\n" +
+                "  position: absolute;\n" +
+                "  content: \"\";\n" +
+                "  height: 18px;\n" +
+                "  width: 18px;\n" +
+                "  left: 4px;\n" +
+                "  bottom: 3px;\n" +
+                "  background-color: white;\n" +
+                "  -webkit-transition: .4s;\n" +
+                "  transition: .4s;\n" +
+                "}\n" +
+                "\n" +
+                "input:checked + .slider {\n" +
+                "  background-color: #2196F3;\n" +
+                "}\n" +
+                "\n" +
+                "input:focus + .slider {\n" +
+                "  box-shadow: 0 0 1px #2196F3;\n" +
+                "}\n" +
+                "\n" +
+                "input:checked + .slider:before {\n" +
+                "  -webkit-transform: translateX(26px);\n" +
+                "  -ms-transform: translateX(26px);\n" +
+                "  transform: translateX(26px);\n" +
+                "}\n" +
+                "\n" +
+                "/* Rounded sliders */\n" +
+                ".slider.round {\n" +
+                "  border-radius: 35px;\n" +
+                "}\n" +
+                "\n" +
+                ".slider.round:before {\n" +
+                "  border-radius: 50%;\n" +
+                "}</style>" +
+                "<script type=text/javascript>" +
+                "function validate() {" +
+                "if (document.getElementById('username').value==''){" +
+                "alert('Please enter username');" +
+                "document.getElementById('username').focus();" +
+                "} else {" +
+                "document.getElementById('billGenerator').submit();" +
+                "}}" +
+                "</script>" +
+                "\t</head>\n" +
+                "<center><b><h4>Generate Defectives List</h4></b></center><hr>" +
+                "\t\t\t\t<div class=\"form-group\">" +
+                "Please enter userId to proceed further.<br><hr>" +
+                "<form name='billGenerator' id='billGenerator' method='POST' action='/app/v2/defects/prelogin'>" +
+                "<center>" +
+                "<div class=\"container\" style=\"width:500px; align:center;\">\n" +
+               " <input class=form-control placeholder=username type=text id=username name=username></div>" +
+               "<div class=\"col-sm-2\" style=\"margin-left:0px;\" id=error-username></div>" +
+               "\t\t\t\t<hr><div class=\"form-group\">\n" +
+               "\t\t\t\t  <div class=\"col-sm-12\">  " +
+               "<input class='btn btn-default' style='align:right;' name='Login' " +
+               "value='Login' type=button onclick=javascript:validate()></div></div>" +
+               "</table></center>" +
+                "" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>";
+    }
+    
     public static String getFirstPage() {
         return "<html>" +
                 "<title>Login Page</title>" +
