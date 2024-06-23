d5e15025aa0922d327988337f208208e70a8a074
satishjajula <jajulasatish08@gmail.com>
2024-06-24 00:20:38 +0530

    add maven plugin

--- src/main/java/com/defectlist/inwarranty/Test.java
+++ src/main/java/com/defectlist/inwarranty/Test.java
@@ -19,10 +19,10 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Test {
-    public static void main(String[] args) {
-
-        test();
-    }
+//    public static void main(String[] args) {
+//
+//        test();
+//    }
 
 
     private static void test() {
